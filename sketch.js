e9826a13b90c0c281b444685b99683915d89d75b
ShivamThakur2411 <balramthakur880@gmail.com>
2020-09-08 15:53:34 +0530

    made a sprite and a camera

--- sketch.js
+++ sketch.js
@@ -1,5 +1,5 @@
 var canvas, backgroundImage;
-
+var cars, car1, car2, car3, car4;
 var gameState = 0;
 var playerCount;
 var allPlayers;
@@ -8,9 +8,8 @@ var database;
 
 var form, player, game;
 
-
 function setup(){
-  canvas = createCanvas(400,400);
+  canvas = createCanvas(displayWidth - 20,displayHeight - 30);
   database = firebase.database();
   game = new Game();
   game.getState();
